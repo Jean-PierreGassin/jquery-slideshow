545a08d33b04111a653ec0e3529d857fe2a23729
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2014-11-03 19:03:58 +1000

    Add closure to scripts/scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,38 +1,44 @@
 //  Simple Slider by Jean-Pierre Gassin
 //  http://jpgassin.com
 
-// Set up a count to see if we have reached our maximum number of images in an array
-var imageCount = $('.sliderImage').length;
+$(document).ready(function() {
+	//Wrap our code in a closure
+	(function() {
 
-// Set the amout of time in between fading in and out (set to your needs)
-var fadeInDelay = 3000;
-var fadeOutDelay = 1770;
+		// Set up a count to see if we have reached our maximum number of images in an array
+		var imageCount = $('.sliderImage').length;
 
-// Initialise the functions
-fadeInSlides();
-fadeOutSlides();
+		// Set the amout of time in between fading in and out (set to your needs)
+		var fadeInDelay = 3000;
+		var fadeOutDelay = 1770;
 
-// Loops through each image and fades them in
-function fadeInSlides() {
-	$('.sliderImage').each(function(i) {
-		$(this).delay(fadeInDelay*i).fadeIn('slow', function() {
-			//console.log("Current Index: " + i + "    Image Count: " + imageCount);
+		// Initialise the functions
+		fadeInSlides();
+		fadeOutSlides();
 
-			// When we've reached the end of the index, execute the loop again
-			if (i == imageCount-1) {
-	    		//console.log("We've reached the if statement!");
-	        	setTimeout(fadeInSlides, 2400);
-	        	setTimeout(fadeOutSlides, 2400);
-			}
-    	});
-	});
-}
+		// Loops through each image and fades them in
+		function fadeInSlides() {
+			$('.sliderImage').each(function(i) {
+				$(this).delay(fadeInDelay*i).fadeIn('slow', function() {
+					//console.log("Current Index: " + i + "    Image Count: " + imageCount);
 
-// Loops through each image and fades them out
-function fadeOutSlides() {
-	$('.sliderImage').each(function(i) {
-		$(this).delay(fadeOutDelay).fadeOut('slow', function() {
-        	$(this).fadeOut('slow');
-    	});
-	});
-}
+					// When we've reached the end of the index, execute the loop again
+					if (i == imageCount-1) {
+			    		//console.log("We've reached the if statement!");
+			        	setTimeout(fadeInSlides, 2400);
+			        	setTimeout(fadeOutSlides, 2400);
+					}
+		    	});
+			});
+		}
+
+		// Loops through each image and fades them out
+		function fadeOutSlides() {
+			$('.sliderImage').each(function(i) {
+				$(this).delay(fadeOutDelay).fadeOut('slow', function() {
+		        	$(this).fadeOut('slow');
+		    	});
+			});
+		}
+	}());
+});
