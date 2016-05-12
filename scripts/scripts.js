2dcde5c810f2e761b34d5f8da162099210516550
jeanpierre.gassin@gmail.com <jeanpierre.gassin@gmail.com>
2016-05-12 16:40:25 +1000

    update to object literal notation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,43 +2,33 @@
 //  http://jpgassin.com
 
 $(document).ready(function() {
-	//Wrap our code in a closure
-	(function() {
+	var simpleSlider = {
+		// Configuration
+		imageCount: $('.sliderImage').length,
+		fadeInDelay: 3000,
+		fadeOutDelay: 1770,
 
-		// Set up a count to see if we have reached our maximum number of images in an array
-		var imageCount = $('.sliderImage').length;
-
-		// Set the amout of time in between fading in and out (set to your needs)
-		var fadeInDelay = 3000;
-		var fadeOutDelay = 1770;
-
-		// Initialise the functions
-		fadeInSlides();
-		fadeOutSlides();
-
-		// Loops through each image and fades them in
-		function fadeInSlides() {
+		fadeInSlides: function() {
 			$('.sliderImage').each(function(i) {
-				$(this).delay(fadeInDelay*i).fadeIn('slow', function() {
-					//console.log("Current Index: " + i + "    Image Count: " + imageCount);
-
-					// When we've reached the end of the index, execute the loop again
-					if (i == imageCount-1) {
-			    		//console.log("We've reached the if statement!");
-			        	setTimeout(fadeInSlides, 2400);
-			        	setTimeout(fadeOutSlides, 2400);
+				$(this).delay(simpleSlider.fadeInDelay * i).fadeIn('slow', function() {
+					// When we've reached the end of the array, execute the loop again
+					if (i == simpleSlider.imageCount -1) {
+						setTimeout(simpleSlider.fadeInSlides, 2400);
+						setTimeout(simpleSlider.fadeOutSlides, 2400);
 					}
-		    	});
+				});
 			});
-		}
+		},
 
-		// Loops through each image and fades them out
-		function fadeOutSlides() {
+		fadeOutSlides: function() {
 			$('.sliderImage').each(function(i) {
-				$(this).delay(fadeOutDelay).fadeOut('slow', function() {
-		        	$(this).fadeOut('slow');
-		    	});
+				$(this).delay(simpleSlider.fadeOutDelay).fadeOut('slow', function() {
+					$(this).fadeOut('slow');
+				});
 			});
 		}
-	}());
+	};
+
+	simpleSlider.fadeInSlides();
+	simpleSlider.fadeOutSlides();
 });
