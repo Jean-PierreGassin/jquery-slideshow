5545e967b2ec2f1239264a791f078c412483c406
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2016-09-15 22:26:59 +1000

    Update indentation to PSR standards

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,33 +2,33 @@
 //  http://jpgassin.com
 
 $(document).ready(function() {
-	var simpleSlider = {
-		// Configuration
-		imageCount: $('.sliderImage').length,
-		fadeInDelay: 3000,
-		fadeOutDelay: 1770,
+    var simpleSlider = {
+        // Configuration
+        imageCount: $('.sliderImage').length,
+        fadeInDelay: 3000,
+        fadeOutDelay: 1770,
 
-		fadeInSlides: function() {
-			$('.sliderImage').each(function(i) {
-				$(this).delay(simpleSlider.fadeInDelay * i).fadeIn('slow', function() {
-					// When we've reached the end of the array, execute the loop again
-					if (i == simpleSlider.imageCount -1) {
-						setTimeout(simpleSlider.fadeInSlides, 2400);
-						setTimeout(simpleSlider.fadeOutSlides, 2400);
-					}
-				});
-			});
-		},
+        fadeInSlides: function() {
+            $('.sliderImage').each(function(i) {
+                $(this).delay(simpleSlider.fadeInDelay * i).fadeIn('slow', function() {
+                    // When we've reached the end of the array, execute the loop again
+                    if (i == simpleSlider.imageCount -1) {
+                        setTimeout(simpleSlider.fadeInSlides, 2400);
+                        setTimeout(simpleSlider.fadeOutSlides, 2400);
+                    }
+                });
+            });
+        },
 
-		fadeOutSlides: function() {
-			$('.sliderImage').each(function(i) {
-				$(this).delay(simpleSlider.fadeOutDelay).fadeOut('slow', function() {
-					$(this).fadeOut('slow');
-				});
-			});
-		}
-	};
+        fadeOutSlides: function() {
+            $('.sliderImage').each(function(i) {
+                $(this).delay(simpleSlider.fadeOutDelay).fadeOut('slow', function() {
+                    $(this).fadeOut('slow');
+                });
+            });
+        }
+    };
 
-	simpleSlider.fadeInSlides();
-	simpleSlider.fadeOutSlides();
+    simpleSlider.fadeInSlides();
+    simpleSlider.fadeOutSlides();
 });
